b46be06b9d17cf5be8a38b979b0a48e8367f7bdc
Yiding Zhang <zhyding12@gmail.com>
2019-10-25 14:37:41 -0700

    initialization

---
+++ src/utils/ListNode.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class ListNode {
+
+    public int val = 0;
+    public ListNode next = null;
+
+    public ListNode (int val) {
+        this.val = val;
+    }
+}
---
+++ src/utils/TreeNode.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class TreeNode {
+
+    public int val = 0;
+    public TreeNode left = null;
+    public TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}

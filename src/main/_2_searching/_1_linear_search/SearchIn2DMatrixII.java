ceae9812bbd1d1ea81217e78592afcf6c1fe5b8c
Yiding Zhang <zhyding12@gmail.com>
2019-10-27 14:00:54 -0700

    search in 2D matrix I & II

--- src/main/_2_searching/_1_linear_search/SearchIn2DMatrixII.java
+++ src/main/_2_searching/_1_linear_search/SearchIn2DMatrixII.java
@@ -1,4 +1,19 @@
 package _2_searching._1_linear_search;
 
 public class SearchIn2DMatrixII {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        if (m == 0) return false;
+        int n = matrix[0].length;
+        if (n == 0) return false;
+
+        int i = m - 1;
+        int j = 0;
+        while (i >= 0 && j < n) {
+            if (matrix[i][j] == target) return true;
+            else if (matrix[i][j] > target) i--;
+            else j++;
+        }
+        return false;
+    }
 }
--- src/main/_2_searching/_2_binary_search/SearchIn2DMatrix.java
+++ src/main/_2_searching/_2_binary_search/SearchIn2DMatrix.java
@@ -1,4 +1,39 @@
 package _2_searching._2_binary_search;
 
 public class SearchIn2DMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0) return false;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        if (target < matrix[0][0] || target > matrix[m - 1][n - 1]) return false;
+
+        int lo = 0;
+        int hi = m - 1;
+        // search by row
+        while (lo < hi) {
+            int mid = lo + (hi - lo + 1) / 2;
+            if (matrix[mid][0] == target) return true;
+            else if (matrix[mid][0] > target) {
+                hi = mid - 1;
+            } else {
+                lo = mid;
+            }
+        }
+
+        int r = lo;
+        lo = 0;
+        hi = n - 1;
+        // search by column
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (matrix[r][mid] == target) return true;
+            else if (matrix[r][mid] > target) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        return matrix[r][lo] == target;
+    }
 }

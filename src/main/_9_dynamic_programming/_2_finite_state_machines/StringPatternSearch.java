d2b78f04f0fcd81ab0daa8f11f5d691f571a9ece
Yiding Zhang <zhyding12@gmail.com>
2019-10-30 21:19:13 -0700

    KMP algorithm

--- src/main/_9_dynamic_programming/_2_finite_state_machines/StringPatternSearch.java
+++ src/main/_9_dynamic_programming/_2_finite_state_machines/StringPatternSearch.java
@@ -1,5 +1,40 @@
 package _9_dynamic_programming._2_finite_state_machines;
 
+import java.util.Arrays;
+
 // KMP algorithm using DFA
 public class StringPatternSearch {
+
+    public boolean containsPattern(String s, String p) {
+        if (p.length() == 0) return true;
+        if (s.length() == 0) return false;
+
+        int[][] dfa = new int[128][p.length()];
+        // build dfa
+        dfa[p.charAt(0)][0] = 1;
+        for (int i = 1, x = 0; i < p.length(); i++) {
+            for (int c = 0; c < 26; c++) {
+                dfa[c][i] = dfa[c][x];
+            }
+            dfa[p.charAt(i)][i] = i + 1;
+            x = dfa[p.charAt(i)][x];
+        }
+
+        return search(s, p, dfa);
+    }
+
+    public boolean search(String s, String p, int[][] dfa) {
+        int index = 0;
+        for (char c : s.toCharArray()) {
+            index = dfa[c][index];
+            if (index == p.length()) return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String s = "The quick brown fox jumps over the lazy dog";
+        String p = "quick brown";
+        System.out.println(new StringPatternSearch().containsPattern(s, p));
+    }
 }

5ad9c13c5702f9136f119ddb407b0a5ed8c86757
Yiding Zhang <zhyding12@gmail.com>
2019-11-02 23:29:01 -0700

    binary indexed tree

---
+++ src/main/_5_binary_trees/_6_binary_indexed_tree/BinaryIndexedTree.java
@@ -0,0 +1,13 @@
+package _5_binary_trees._6_binary_indexed_tree;
+
+/**
+ * Store sequence segments of the length of 2's multiple
+ *
+ * Average case:
+ *   Both read and write within O(log(N))
+ *
+ * Worst case:
+ *   Both read and write within O(log(N))
+ */
+public class BinaryIndexedTree {
+}

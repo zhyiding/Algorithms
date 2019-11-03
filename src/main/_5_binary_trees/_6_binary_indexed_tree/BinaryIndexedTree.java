3c1d3fd1b600ffdd6f08bfe3e04b665917a82421
Yiding Zhang <zhyding12@gmail.com>
2019-11-02 23:36:59 -0700

    binary indexed tree

--- src/main/_5_binary_trees/_6_binary_indexed_tree/BinaryIndexedTree.java
+++ src/main/_5_binary_trees/_6_binary_indexed_tree/BinaryIndexedTree.java
@@ -10,4 +10,20 @@ package _5_binary_trees._6_binary_indexed_tree;
  *   Both read and write within O(log(N))
  */
 public class BinaryIndexedTree {
+
+    // Gets parent index to read
+    private static int getParentIndex(int current) {
+        int complement = ~current + 1;
+        return current - (current & complement);
+    }
+
+    // Gets next index to update
+    private static int getNextIndex(int current) {
+        int complement = ~current + 1;
+        return current + (current & complement);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getNextIndex(6));
+    }
 }
